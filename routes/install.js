56612564eab31bb0bb3ca4b7989c4a486a2938b9
Sunny Jirakit <costerraid@Sunnys-MacBook-Air.local>
2023-03-08 13:53:28 +0700

    Install System Update

--- routes/install.js
+++ routes/install.js
@@ -6,7 +6,7 @@ module.exports = (sha256) => {
     const password = sha256(process.env.DEFAULT_PASSWORD)
     //SQL Commands
     sql1 = "CREATE TABLE contents(id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, title VARCHAR(200) NOT NULL, contents MEDIUMTEXT NOT NULL, category VARCHAR(50) NOT NULL, writter VARCHAR(50) NOT NULL, cover VARCHAR(200) DEFAULT '/image/default.jpeg', is_show INT(1) DEFAULT 0 NOT NULL)"
-    sql2 = "CREATE TABLE admin(id INT(6) AUTO_INCREMENT PRIMARY KEY, username VARCHAR(100) NOT NULL, password VARCHAR(200) NOT NULL)"
+    sql2 = "CREATE TABLE admin(id INT(6) AUTO_INCREMENT PRIMARY KEY, username VARCHAR(100) NOT NULL UNIQUE, password VARCHAR(200) NOT NULL)"
     sql3 = "INSERT INTO admin(username,password) VALUES(?,?)"
     sql4 = "CREATE TABLE profile(id INT(6) AUTO_INCREMENT PRIMARY KEY, admin INT(6) NOT NULL, name VARCHAR(50) NOT NULL, bio VARCHAR(500), picture VARCHAR(200) DEFAULT '/image/default_profile.png')"
     //Install Contents Table.
@@ -24,13 +24,8 @@ module.exports = (sha256) => {
         if(err) throw err;
         timeStamp("[+] Create Table profile Successful!")
     })
-    //Check If admin account is exist or create new one using .env details.
-    db.query('SELECT * FROM admin', (err,result) => {
-        if(result.length == 0) {
-            db.query(sql3, [username,password], (err,result) => {
-                if(err) throw err;
-                timeStamp("[+] Create admin account Successful!")
-            })
-        }
+    db.query(sql3, [username,password], (err,result) => {
+        if(err) throw err;
+        timeStamp("[+] Create admin account Successful!")
     })
 }
